defbc57d1ba06c7e666d19ee882d9d114e5299af
Gary Green <Gary.Green@jfs.ohio.gov>
2019-05-08 09:35:48 -0400

    Update ODAPS_DB from PSQL1 (Schema compare)

---
+++ ODAPS_DB/dbo/Views/vw_APS_Case_Worker_Sup_ba.sql
@@ -0,0 +1,9 @@
+﻿
+CREATE VIEW [dbo].[vw_APS_Case_Worker_Sup_ba]
+AS
+SELECT        Staging.Cases.ID, Staging.Contacts.Name AS Client, Staging.Contacts.Salutation, Staging.Contacts.MailingStreet, Staging.Contacts.MailingCity, Staging.Contacts.MailingStateCode, Staging.Contacts.MailingPostalCode, 
+                         SW.Name AS Social_Worker, Sup.Name AS Supervisor
+FROM            Staging.Cases INNER JOIN
+                         dbo.Users AS SW ON Staging.Cases.OwnerId = SW.UserID LEFT OUTER JOIN
+                         Staging.Contacts ON Staging.Cases.Reporting_Party_Name__c = Staging.Contacts.ID LEFT OUTER JOIN
+                         dbo.Users AS Sup ON SW.SupervisorID = Sup.UserID
