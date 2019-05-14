6a884bde4f96719cbbf97240507566bbda16ba61
Gary Green <Gary.Green@jfs.ohio.gov>
2019-05-14 14:01:15 -0400

    End of day.  TransformLoad Accounts and Users working

--- ODAPS_DB/Staging/Tables/Users.sql
+++ ODAPS_DB/Staging/Tables/Users.sql
@@ -1,27 +1,33 @@
 ﻿CREATE TABLE [Staging].[Users] (
     [UserKey]          INT            IDENTITY (1, 1) NOT NULL,
-    [SysInsertUser]    VARCHAR (50)   CONSTRAINT [DF__Users__SysInsert__5812160E] DEFAULT ('System') NOT NULL,
-    [SysInsertDate]    DATETIME       CONSTRAINT [DF__Users__SysInsert__59063A47] DEFAULT (getdate()) NOT NULL,
+    [SysInsertUser]    VARCHAR (50)   CONSTRAINT [DF_Users_SysInsertUser] DEFAULT ('System') NOT NULL,
+    [SysInsertDate]    DATETIME       CONSTRAINT [DF_Users_SysInsertDate] DEFAULT (getdate()) NOT NULL,
+
     [ID]               VARCHAR (18)   NOT NULL,
     [CreatedByID]      CHAR (18)      NULL,
     [CreatedDate]      DATE           NULL,
     [LastModifiedById] CHAR (18)      NULL,
     [LastModifiedDate] DATE           NULL,
     [SystemModstamp]   DATETIME       NULL,
+
     [AccountID]        VARCHAR (18)   NULL,
     [ContactID]        VARCHAR (18)   NULL,
+
     [UserName]         VARCHAR (80)   NULL,
     [FirstName]        VARCHAR (40)   NULL,
     [LastName]         VARCHAR (80)   NULL,
     [Name]             VARCHAR (128)  NULL,
     [EMail]            VARCHAR (128)  NULL,
     [Phone]            VARCHAR (40)   NULL,
+
     [ManagerID]        VARCHAR (18)   NULL,
     [Supervisor__c]    VARCHAR (18)   NULL,
+
     [IsValid]          BIT            CONSTRAINT [DF_Users_IsValid] DEFAULT ((1)) NOT NULL,
     [IsLoaded]         BIT            CONSTRAINT [DF_Users_IsLoaded] DEFAULT ((0)) NOT NULL,
-    [LoadDate]         DATETIME       NULL,
+    [LoadDate]         DATETIME       CONSTRAINt [DF_Users_LoadDate] DEFAULT '1900-01-01' NOT NULL,
     [Comments]         VARCHAR (1024) NULL,
+
     CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([UserKey] ASC)
 );
 
---
+++ ODAPS_DB/dbo/Tables/Accounts.sql
@@ -0,0 +1,29 @@
+﻿CREATE TABLE [dbo].[Accounts]
+(
+	[AccountKey] [int] IDENTITY(1,1) NOT NULL,
+	[SysInsertUser] [varchar](50) NOT NULL DEFAULT 'Sys_ETL',
+	[SysInsertDate] [datetime] NOT NULL,
+
+	[ID] [char](18),
+	[CreatedByID] [char](18) NULL,
+	[CreatedDate] [date],
+	[LastModifiedById] [char](18) NULL,
+	[LastModifiedDate] [date]
+	[SystemModstamp] [date]
+	[IsDeleted] [bit],
+
+	[MasterRecordID] [char](18) NULL,
+	[OwnerID] [char](18) NULL,
+	[ParentID] [char](18) NULL,
+	[RecordTypeID] [char](18) NULL,
+	[Type] [varchar](40) NULL,
+
+	[Name] [varchar](255) NULL,
+	[Description] [varchar](4096) NULL,
+
+	[LastActivityDate] [date] NULL,
+	[LastViewedDate][date] NULL,
+	[LastReferencedDate] [date] NULL,
+
+	CONSTRAINT [PK_Accounts] PRIMARY KEY CLUSTERED ([AccountKey] ASC)
+)
