ffc14c824586dcc745dab9cf4e81f8a7bcb29615
Gary Green <Gary.Green@jfs.ohio.gov>
2019-05-29 09:32:15 -0400

    A  few items

--- _SolutionItems/ProbeODAPS.bat
+++ _SolutionItems/ProbeODAPS.bat
@@ -1,1 +1,1 @@
-\\MS-CJFS-SIS\Source\Development\Packages\Extract-ODAPS\ProbeODAPS.exe Extract -o Account User Contact APS_Case__c --Path \\MS-CJFS-SIS\Source\Development\Working\Extract-ODAPS
+\\MS-CJFS-SIS\Source\Production\Packages\Extract-ODAPS\ProbeODAPS.exe Extract -o Account User Contact APS_Case__c --Path \\MS-CJFS-SIS\Source\Production\Working\Extract-ODAPS
