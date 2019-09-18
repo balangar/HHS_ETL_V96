fc96207211e0e0547d0354d7b43ffa2815cb933c
Gary Green <Gary.Green@jfs.ohio.gov>
2019-09-18 09:17:59 -0400

    Add bare-bones project

---
+++ Prepare_FinancialNotes/Main.cs
@@ -0,0 +1,23 @@
+﻿namespace Prepare_FinancialNotes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading.Tasks;
+    using log4net;
+
+    internal class Main
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        internal static int Work(string[] args)
+        {
+            int exitStatus = 0;
+
+            return exitStatus;
+
+        }
+    }
+}
