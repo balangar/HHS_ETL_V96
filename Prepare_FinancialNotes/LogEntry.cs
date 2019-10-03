6b59bbfbd5526a6d9d640031f2ff3547aa5e1043
Gary Green <Gary.Green@jfs.ohio.gov>
2019-10-03 14:27:07 -0400

    WIP.  End of day. ReceiptLogEntry

--- Prepare_FinancialNotes/LogEntry.cs
+++ Prepare_FinancialNotes/LogEntry.cs
@@ -4,20 +4,21 @@ namespace Prepare_FinancialNotes
 {
     internal abstract class BaseLogEntry
     {
-        internal XmlOutput xo;
-        internal abstract XmlOutput GetXML();
+        internal abstract XmlOutput GetXML(XmlOutput XO);
     }
     internal class BlockHeader : BaseLogEntry
     {
         string OrderNo { get; }
         string BlockDate { get; }
-        internal override XmlOutput GetXML()
+        internal override XmlOutput GetXML(XmlOutput XO)
         {
-            return( new XmlOutput()
+            return (
+                XO
                 .XmlDeclaration()
                 .Node("FinRecord").Within()
                 .Attribute("OrderNumber", OrderNo)
-                .Attribute("Date", BlockDate));
+                .Attribute("Date", BlockDate)
+                );
 
         }
         internal BlockHeader(string OrderNumber, string BlockDate)
@@ -28,16 +29,27 @@ namespace Prepare_FinancialNotes
     }
     internal class ReceiptLogEntry : BaseLogEntry
     {
-        const int AMOUNT_RECEIVED_OFFSET = 18; const int AMOUNT_RECEIVED_LENGTH = 8;
-
+        const int PAYEE_TYPE_OFFSET = 47; const int PAYEE_TYPE_LENGTH = 3;
+        const int AMOUNT_RECEIVED_OFFSET = 61; const int AMOUNT_RECEIVED_LENGTH = 8;
+        const int ALTERNATE_PAYEE_OFFSET = 50; const int ALTERNATE_PAYEE_LENGTH = 10;
+        
+        string PayeeType { get; }
         string AmountReceived { get; }
-        internal override XmlOutput GetXML()
+        string AlternatePayee { get; }
+        internal override XmlOutput GetXML(XmlOutput XO)
         {
-            throw new NotImplementedException();
+            return (
+                XO
+                .Node("RECEIPT")
+                .Attribute("AmountRecived", AmountReceived)
+                .Attribute("AlternatePayee", AlternatePayee)
+                );
         }
         internal ReceiptLogEntry(string RecordText) : base()
         {
+            PayeeType = RecordText.Substring(PAYEE_TYPE_OFFSET, PAYEE_TYPE_LENGTH);
             AmountReceived = RecordText.Substring(AMOUNT_RECEIVED_OFFSET, AMOUNT_RECEIVED_LENGTH);
+            AlternatePayee = RecordText.Substring(ALTERNATE_PAYEE_OFFSET, ALTERNATE_PAYEE_LENGTH);
         }
 
     }
