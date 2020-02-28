9858828aa104c11111ed6c124912884bc184bda9
Gary Green <Gary.Green@jfs.ohio.gov>
2020-02-28 13:53:25 -0500

    Staging
    Basic load of staging table done

--- DB_SheriffMatch/Staging/Tables/OBWP_JailMatch.sql
+++ DB_SheriffMatch/Staging/Tables/OBWP_JailMatch.sql
@@ -1,27 +1,30 @@
 ﻿CREATE TABLE [Staging].[OBWP_JailMatch] (
-    [ID]                        INT              NOT NULL IDENTITY (1, 1) PRIMARY KEY,
-    [SysInsertUser]             VARCHAR (50)     NULL,
-    [SysInsertDate]             DATETIME         NOT NULL DEFAULT getdate(),
+    [ID]                        INT              IDENTITY (1, 1) NOT NULL,
+    [SysInsertUser]             VARCHAR (50)     DEFAULT ('System') NOT NULL,
+    [SysInsertDate]             DATETIME         DEFAULT (getdate()) NOT NULL,
     [HashSignature]             VARBINARY (8000) NULL,
-    [ResidenceCountyCode]       CHAR (2)         NOT NULL DEFAULT '18', 
+    [ResidenceCountyCode]       CHAR (2)         NULL,
     [SocSecNo]                  CHAR (9)         NULL,
     [ApplicantStatusCode]       VARCHAR (50)     NULL,
     [FirstName]                 VARCHAR (50)     NULL,
     [LastName]                  VARCHAR (50)     NULL,
     [GenderCode]                VARCHAR (50)     NULL,
-    [BirthDate]                 DATE             NULL,
-    [Age]                       INT              NULL,
+    [BirthDate]                 VARCHAR (50)     NULL,
+    [Age]                       VARCHAR (50)     NULL,
     [CaseSerialNumber]          VARCHAR (50)     NULL,
     [ProgramCode]               VARCHAR (50)     NULL,
     [CaseWorkerNumber]          VARCHAR (50)     NULL,
-    [BDAAidBeginDate]           DATE             NULL,
-    [AidCodeEndDate]            DATE             NULL,
+    [BDAAidBeginDate]           VARCHAR (50)     NULL,
+    [AidCodeEndDate]            VARCHAR (50)     NULL,
     [ApplicantID]               VARCHAR (50)     NULL,
     [BillingNumber]             VARCHAR (50)     NULL,
     [LivingArrangementTypeCode] VARCHAR (50)     NULL,
-    [IsValid]                   BIT              NOT NULL DEFAULT 1, 
-    [IsLoaded]                  BIT              NOT NULL DEFAULT 0, 
-    [LoadDate]                  DATETIME         NOT NULL DEFAULT '1900-01-01',
-    [Comments]                  VARCHAR (1024)   NULL
+    [IsValid]                   BIT              DEFAULT ((1)) NOT NULL,
+    [IsLoaded]                  BIT              DEFAULT ((0)) NOT NULL,
+    [LoadDate]                  DATETIME         DEFAULT ('1900-01-01') NOT NULL,
+    [Comments]                  VARCHAR (1024)   NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
 );
+
+
 
