a49865de65949de1bd67807f2cf441e25d3a2744
Gary green <Gary.Green@jfs.ohio.gov>
2020-03-09 11:03:47 -0400

    Housecleaning
    Modify name of input csv file.  Misc. odds and ends.

--- DB_SheriffMatch/Staging/Tables/OBWP_JailMatch.sql
+++ DB_SheriffMatch/Staging/Tables/OBWP_JailMatch.sql
@@ -4,7 +4,7 @@
     [SysInsertDate]             DATETIME         DEFAULT (getdate()) NOT NULL,
     [HashSignature]             VARBINARY (8000) NULL,
     [ResidenceCountyCode]       CHAR (2)         NULL,
-    [SocSecNo]                  CHAR (9)         NULL,
+    [SocSecNo]                  VARCHAR (9)      NULL,
     [ApplicantStatusCode]       VARCHAR (50)     NULL,
     [FirstName]                 VARCHAR (50)     NULL,
     [LastName]                  VARCHAR (50)     NULL,
