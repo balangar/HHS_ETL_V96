fa9da27f1c62c89019510251f638ddae9d6e7267
Gary Green <Gary.Green@jfs.ohio.gov>
2020-06-16 07:51:32 -0400

    Basic
    Add basic OMIS project

---
+++ Prepare_OMIS/Main.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System;
+using System.Reflection;
+
+using System.Data.SqlClient;
+
+using log4net;
+
+
+namespace Prepare_OMIS
+{
+    internal class Main
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        internal static int Work()
+        {
+            const string sourcePath = @"\\ms-hhs-psql2\c$\SqlDB\SIS\Source\Stage\Working\LoadOCSS-Archive\_ORDR\DATAOMIS_CleansedAndDelimited.csv";
+
+            return 1;
+        }
+    }
+}
