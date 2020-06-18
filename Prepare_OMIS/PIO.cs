f37d4c60975a6e8d441e03203376983c7566f9a7
Gary Green <Gary.Green@jfs.ohio.gov>
2020-06-18 14:06:04 -0400

    End of day
    WIP.  PIO.  Shred (or is shard?) file.

--- Prepare_OMIS/PIO.cs
+++ Prepare_OMIS/PIO.cs
@@ -52,47 +52,90 @@ namespace Prepare_OMIS
             StrReader = null;
         }
 
-        internal static void GetRecordLists(out List<OT01> Type1Records)
+        internal static void GetSeparatedRecordTypes(out List<OT01> T1Records, out List<OT02> T2Records, out List<OT03> T3Records )
         {
             var r1 = new List<OT01>();
             var r2 = new List<OT02>();
             var r3 = new List<OT03>();
 
+            var dr1 = new Dictionary<string, OT01>();
+            var dup = new Dictionary<string, int>();
+
+            string previousCaseNumber = string.Empty;
+            string currentCaseNumber = string.Empty;
+
+            //TODO:  Use Case Number + sequence number as key to avoid "duplicates".
+
             while (CsReader.Read())
             {
+                currentCaseNumber = CsReader.GetField(1);
+                switch (CsReader.GetField(3))
                 {
-                    switch (CsReader.GetField(3))
-                    {
-                        case "01":
-                            r1.Add(CsReader.GetRecord<OT01>());
-                            break;
+                    case "01":
+                        if(!dr1.TryGetValue(currentCaseNumber, out _))
+                        {
+                            dr1.Add(currentCaseNumber, CsReader.GetRecord<OT01>());
+                        }
+                        else
+                        {
+                            Console.WriteLine(currentCaseNumber);
+                        }
+                       r1.Add(CsReader.GetRecord<OT01>());
+                       break;
 
-                        case "02":
-                            r2.Add(CsReader.GetRecord<OT02>());
-                            break;
+                    //case "02":  // For reasons I don't understand, we miss the first (and only the first) type 02 record.  [geg] 2020-06-18
+                    //    r2.Add(CsReader.GetRecord<OT02>());
+                    //    break;
 
-                        case "03":
-                            r3.Add(CsReader.GetRecord<OT03>());
-                            break;
+                    //case "03":  // spot checking file suggests that when there are multiple 03 records all are duplicates of the first. [geg] 2020-06-18
+                    //    if(currentCaseNumber != previousCaseNumber)
+                    //    {
+                    //        previousCaseNumber = currentCaseNumber;
+                    //        r3.Add(CsReader.GetRecord<OT03>());
+                    //    }
+                    //    break;
 
-                        default:
-                            break;
-                    }
+                    default:
+                        break;
+                        //throw new InvalidOperationException("Unknown record type: " + CsReader.GetField(3));
                 }
+                
             }
-            Type1Records = r1;
-
+            T1Records = r1;
+            T2Records = r2;
+            T3Records = r3;
         }
 
         internal static void CreateRecordsDictionary()
         {
             var r1 = new List<OT01>();
+            var r2 = new List<OT02>();
+            var r3 = new List<OT03>();
 
-            var r = new Dictionary<string, Tuple<OT01>>();
+            var r = new Dictionary<string, OMISRecord>();
 
-            GetRecordLists(out r1);
+            GetSeparatedRecordTypes(out r1, out r2, out r3 );
+
+            var dr01 = r1
+                .GroupBy(p => p.CaseNumber, StringComparer.OrdinalIgnoreCase)
+                .ToDictionary(g => g.Key, g => g.First(), StringComparer.OrdinalIgnoreCase);
+
+            List<string> c1List = new List<string>();
+            foreach (var e in r1)
+            {
+                c1List.Add(e.CaseNumber);
+            }
+
+            var keyList = dr01.Keys.ToList();
+
+            var list3 = c1List.Except(keyList);
 
 
+
+            var dr02 = r2
+                .GroupBy(p => p.CaseNumber, StringComparer.OrdinalIgnoreCase)
+                .ToDictionary(g => g.Key, g => g.First(), StringComparer.OrdinalIgnoreCase);
+       
         }
 
         public static void GetNextRecord()
@@ -170,6 +213,25 @@ namespace Prepare_OMIS
             Map(m => m.CaseNumber).Index(1);
             Map(m => m.Comment).Index(4);
         }
+    }
+    public class OMISRecord
+    {
+        public string OrderFilingDate { get; set; }
+        public string LastModifyDate { get; set; }
+        public string WageAttStartDate { get; set; }
+        public string WageAttEndDate { get; set; }
+        public string OrderReviewDate { get; set; }
+        public string AbeyanceIndicator { get; set; }
+        public string OutOfStateSONum { get; set; }
+
+        public string AmountOrderHold { get; set; }
+        public string AmountODTHold { get; set; }
+        public string AmountIRSHold { get; set; }
+        public string AmountIRSJointHold { get; set; }
+        public string AmountLumpHold { get; set; }
+        public string LastCalcDate { get; set; }
+
+        public string Comments { get; set; }
     }
 
 }
